c847eb63456cf2dd2a6ce89b33f9828d5bed58af
Emmanuel Mensah <ninodonoyiti2@gmail.com>
2024-01-02 22:18:10 +0000

    Quantity added to cartPage

--- lib/pages/cartPage.dart
+++ lib/pages/cartPage.dart
@@ -13,18 +13,54 @@ class CartPage extends StatefulWidget {
 }
 
 class _CartPageState extends State<CartPage> {
-  //remove from cart
   void removeFromCart(Coffee coffee) {
     Provider.of<CoffeeShop>(context, listen: false).removeFromCart(coffee);
-    showDialog(
-      context: context,
-      builder: (
-        context,
-      ) =>
-          const AlertDialog(
-        title: Text(
-          " Removed from cart ",
-          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<CoffeeShop>(
+      builder: (context, value, child) => SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Column(
+            children: [
+              const Text(
+                "Your cart",
+                style: TextStyle(fontSize: 20),
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: value.userCart.length,
+                  itemBuilder: (context, index) {
+                    Coffee eachCoffee = value.userCart[index];
+                    return CoffeeTile(
+                      coffee: eachCoffee,
+                      onPressed: () => removeFromCart(eachCoffee),
+                      icon: const Icon(Icons.delete),
+                    );
+                  },
+                ),
+              ),
+              GestureDetector(
+                onTap: payNow,
+                child: Container(
+                  padding: const EdgeInsets.all(25),
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.brown,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: const Center(
+                    child: Text(
+                      "Pay now",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -34,57 +70,7 @@ class _CartPageState extends State<CartPage> {
     Navigator.push(
       context,
       MaterialPageRoute(
-          builder: (context) =>
-              CheckoutPage(purchasedItems: Provider.of<CoffeeShop>(context, listen: false).userCart)),
+        builder: (context) => CheckoutPage(purchasedItems:  Provider.of<CoffeeShop>(context, listen: false).userCart)),
     );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Consumer<CoffeeShop>(
-        builder: (context, value, child) => SafeArea(
-                child: Padding(
-              padding: const EdgeInsets.all(25.0),
-              child: Column(
-                children: [
-                  //heading
-                  const Text(
-                    "Your cart",
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  //list of cart
-                  Expanded(
-                    child: ListView.builder(
-                        itemCount: value.userCart.length,
-                        itemBuilder: (context, index) {
-                          //get individual coffee items
-                          Coffee eachCoffee = value.userCart[index];
-
-                          //return coffee tile
-                          return CoffeeTile(
-                              coffee: eachCoffee,
-                              onPressed: () => removeFromCart(eachCoffee),
-                              icon: const Icon(Icons.delete));
-                        }),
-                  ),
-                  GestureDetector(
-                    onTap: payNow,
-                    child: Container(
-                      padding: const EdgeInsets.all(25),
-                      width: double.infinity,
-                      decoration: BoxDecoration(
-                          color: Colors.brown,
-                          borderRadius: BorderRadius.circular(12)),
-                      child: const Center(
-                        child: Text(
-                          "Pay now",
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            )));
   }
 }
